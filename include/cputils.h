75d5fde46c8901c849bb8c2b154e43118e1ca1dd
Ritik Raj <ritikrajgaya033@gmail.com>
2026-03-09 18:24:03 +0530

    Updated include/cputils.h with new definitions

--- include/cputils.h
+++ include/cputils.h
@@ -3,39 +3,38 @@
 
 #include <vector>
 #include <iostream>
+#include <utility>
 
-using namespace std;
-
-inline void print1d(const vector<int>& v) {
-    for (const auto& el : v) cout << el << " ";
-    cout << "\n";
+inline void print1d(const std::vector<int>& v) {
+    for (const auto& el : v) std::cout << el << " ";
+    std::cout << "\n";
 }
 
-inline void print2d(const vector<vector<int>>& v) {
+inline void print2d(const std::vector<std::vector<int>>& v) {
     for (const auto& row : v) {
-        for (const auto& el : row) cout << el << " ";
-        cout << "\n";
+        for (const auto& el : row) std::cout << el << " ";
+        std::cout << "\n";
     }
 }
 
 template <typename A, typename B>
-inline void printPair(const pair<A, B>& p) {
-    cout << "(" << p.first << ", " << p.second << ")\n";
+inline void printPair(const std::pair<A, B>& p) {
+    std::cout << "(" << p.first << ", " << p.second << ")\n";
 }
 
 template <typename A, typename B>
-inline void printPairs(const vector<pair<A, B>>& v) {
+inline void printPairs(const std::vector<std::pair<A, B>>& v) {
     for (const auto& p : v) printPair(p);
 }
 
 inline void fastIO() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 }
 
 #ifndef ONLINE_JUDGE
-#define dbg(x) cerr << #x << " = " << x << "\n"
-#define dbgv(v) do { cerr << #v << " = [ "; for (const auto& _e : v) cerr << _e << " "; cerr << "]\n"; } while(0)
+#define dbg(x) std::cerr << #x << " = " << x << "\n"
+#define dbgv(v) do { std::cerr << #v << " = [ "; for (const auto& _e : v) std::cerr << _e << " "; std::cerr << "]\n"; } while(0)
 #else
 #define dbg(x)
 #define dbgv(v)
@@ -43,9 +42,9 @@ inline void fastIO() {
 
 typedef long long           ll;
 typedef unsigned long long  ull;
-typedef pair<int, int>      pii;
-typedef vector<int>         vi;
-typedef vector<vector<int>> vvi;
+typedef std::pair<int, int>      pii;
+typedef std::vector<int>         vi;
+typedef std::vector<std::vector<int>> vvi;
 
 inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
 inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
